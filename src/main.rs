05fc2063ba73ed0f24863bed1f143560809c0a59
shinnku <nikaidou@shinnku.com>
2025-05-11 22:56:16 +0800

    add basic download

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use std::collections::HashMap;
-
 mod args;
 use args::Args;
 use clap::Parser;
+use reqwest::Client;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    let mut handles = Vec::new();
+    let client = Client::new();
 
-    let resp = reqwest::get(&args.url)
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{resp:#?}");
+    for _ in 0..args.concurrent_count {
+        let url = args.url.clone();
+        let client = client.clone();
+        let handle = tokio::spawn(async move {
+            if let Ok(resp) = client.get(&url).send().await {
+                let _ = resp.bytes().await;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
     Ok(())
 }
