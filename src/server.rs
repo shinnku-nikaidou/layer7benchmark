337bc4b6f0a160a3d6f674b6b57569934254c599
shinnku <nikaidou@shinnku.com>
2025-05-15 20:41:51 +0800

    fix server

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,12 @@
 use futures_util::{SinkExt, StreamExt};
-use reqwest_websocket::RequestBuilderExt;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 pub async fn connect_to_server() -> anyhow::Result<()> {
-    let client = reqwest::Client::new();
-
-    let resp = client
-        .get("ws://127.0.0.1:3000/ws")
-        .upgrade()
-        .send()
-        .await?;
-
-    let mut ws = resp.into_websocket().await?;
-    ws.send(reqwest_websocket::Message::Text("Hello".into()))
-        .await?;
-    while let Some(msg) = ws.next().await {
-        let msg = msg?;
-        if let reqwest_websocket::Message::Text(txt) = msg {
-            println!("收到广播: {}", txt);
-        }
+    let url = url::Url::parse("ws://127.0.0.1:3000/ws").unwrap();
+    let (mut ws_stream, _resp) = connect_async(url.as_str()).await.unwrap();
+    ws_stream.send(Message::Text("Hello".into())).await.unwrap();
+    if let Some(msg) = ws_stream.next().await {
+        println!("Received: {:?}", msg.unwrap());
     }
     Ok(())
 }
